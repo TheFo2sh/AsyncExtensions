0391765ad7c2241b62682736ea845db3b9a3687c
TheFo2sh <ahmed.fouad.net@hotmail.com>
2019-06-03 12:49:16 +0200

    Update IncrementalLoadingCollection.cs

--- AsyncEnumerableExtensions/IncrementalLoadingCollection.cs
+++ AsyncEnumerableExtensions/IncrementalLoadingCollection.cs
@@ -1,49 +1,49 @@
-using System;
-using System.Collections.Async;
-using System.Collections.ObjectModel;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading;
-using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.UI.Xaml.Data;
+  using System;
+    using System.Collections.Async;
+    using System.Collections.ObjectModel;
+    using System.Runtime.InteropServices.WindowsRuntime;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Windows.Foundation;
+    using Windows.UI.Xaml.Data;
 
-namespace AsyncEnumerableExtensions
-{
-    public class IncrementalLoadingCollection<T> :ObservableCollection<T> , ISupportIncrementalLoading
+    namespace AsyncEnumerableExtensions
     {
-        private readonly IAsyncEnumerable<T>> _asyncEnumerable;
-        private IAsyncEnumerator<T> enumerator;
-
-        internal IncrementalLoadingCollection(IAsyncEnumerable<T> asyncEnumerable)
+        public class IncrementalLoadingCollection<T> : ObservableCollection<T>, ISupportIncrementalLoading
         {
-            _asyncEnumerable=asyncEnumerable;
-        }
+            private readonly IAsyncEnumerable<T> _asyncEnumerable;
+            private IAsyncEnumerator<T> enumerator;
 
-        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
-        {
-            return AsyncInfo.Run((c) => LoadMoreItemsAsync(c, count));
-        }
-
-        private async Task<LoadMoreItemsResult> LoadMoreItemsAsync(CancellationToken c, uint count)
-        {
-          
-            if (enumerator == null)
-                enumerator = await _asyncEnumerable.GetAsyncEnumeratorAsync(c);
-
-            for (var i = 0; i < count; i++)
+            internal IncrementalLoadingCollection(IAsyncEnumerable<T> asyncEnumerable)
             {
-                if (await enumerator.MoveNextAsync(c))
-                    this.Add(enumerator.Current);
-                else
-                {
-                    HasMoreItems = false;
-                    break;
-                }
-
+                _asyncEnumerable = asyncEnumerable;
             }
-            return new LoadMoreItemsResult(){Count = (uint)this.Count};
-        }
 
-        public bool HasMoreItems { get; set; } = true;
+            public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+            {
+                return AsyncInfo.Run((c) => LoadMoreItemsAsync(c, count));
+            }
+
+            private async Task<LoadMoreItemsResult> LoadMoreItemsAsync(CancellationToken c, uint count)
+            {
+
+                if (enumerator == null)
+                    enumerator = await _asyncEnumerable.GetAsyncEnumeratorAsync(c);
+
+                for (var i = 0; i < count; i++)
+                {
+                    if (await enumerator.MoveNextAsync(c))
+                        this.Add(enumerator.Current);
+                    else
+                    {
+                        HasMoreItems = false;
+                        break;
+                    }
+
+                }
+                return new LoadMoreItemsResult() { Count = (uint)this.Count };
+            }
+
+            public bool HasMoreItems { get; set; } = true;
+        }
     }
-}
